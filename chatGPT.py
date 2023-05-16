f77de4a13d1f4b332cc8f5db3975df11a7e2e5ab
Cheng <cxing@bowdoin.edu>
2023-05-16 12:26:40 -0400

    updated process_titles_average_score in GPT

--- chatGPT.py
+++ chatGPT.py
@@ -1,36 +1,66 @@
 import openai
+import scrape as scrap
 
+system_message ="Please return a single numerical score ranging from -100 to +100 for each of the following news article titles for their sector. A score of -100 indicates extreme negative sentiment that could potentially have a disastrous impact on the stock price of that specific sector. A score of 0 is neutral, perhaps neither impacting that market sector positively or negatively. A score of +100 indicates extreme positive sentiment that could potentially have a highly beneficial impact on that sector’s stock price. Make sure that is the impact on that specific sector, not the impact on the macro markets or other non-related markets. If you think that this is not a news article title or will not impact the financial market as a whole, return 0. And remember to provide only numerical responses, no explanation at all, especially when it is 0. \n Here are some examples and guidelines: \n Commodities, Widespread Drought Devastates Global Crop Production, Commodities Prices to Skyrocket, -100; Tech, Tech Industry Under Threat: Major Data Breach Affects All Big-Tech Companies such as Google, Meta, Microsoft, -100; Tech, Stable Quarter Reported for Tech Sector Amid Mixed Market Signals, 0; Tech, Breakthrough in Artificial Intelligence Technology Promises to Catapult Tech Sector to New Highs, 100; Finance, Global Financial Crisis Looms: Interest Rates Surge Unexpectedly, -100; Finance, Sudden Surge in Global IPO Activity: Financial Sector Set for Record Profits, +100"
 def read_api_key(file_path):
     with open(file_path, 'r') as file:
         return file.read().strip()
+
 
 def process_titles(titles_dict):
     output_file = 'scores.txt'
     with open(output_file, 'w') as out_file:
         messages = [
-            {"role": "system", "content": ""
-                                          ""
-                                          "Please return a single numerical score ranging from -100 to +100 for each of the following news article titles for their sector. A score of -100 indicates extreme negative sentiment that could potentially have a disastrous impact on the stock price of that specific sector. A score of 0 is neutral, perhaps neither impacting that market sector positively or negatively. A score of +100 indicates extreme positive sentiment that could potentially have a highly beneficial impact on that sector’s stock price. Make sure that is the impact on that specific sector, not the impact on the macro markets or other non-related markets. If you think that this is not a news article title or will not impact the financial market as a whole, return 0. And remember to provide only numerical responses, no explanation at all, especially when it is 0."
+            {"role": "system", "content": system_message}]
+        sectors = ['Commodities', 'Tech', 'Finance']
+        for key in sectors:
+            if key in titles_dict:
+                title_list = titles_dict[key]
+                out_file.write(f"{key}\n")
+                for title in title_list:
+                    title = title.strip()
+                    if title:
+                        messages.append({"role": "user", "content": f"{key}, {title}"})
+                        try:
+                            chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+                            score = chat.choices[0].message.content
+                            out_file.write(score + '\n')
+                            messages.append({"role": "assistant", "content": score})
+                        except Exception as e:
+                            print(f"An error occurred: {e}")
 
-                                          + "\n"
 
-                                          + "Here are some examples and guidelines: “Commodities, Widespread Drought Devastates Global Crop Production, Commodities Prices to Skyrocket, -100; Tech, Tech Industry Under Threat: Major Data Breach Affects All Big-Tech Companies such as Google, Meta, Microsoft, -100; Tech, Stable Quarter Reported for Tech Sector Amid Mixed Market Signals, 0; Tech, Breakthrough in Artificial Intelligence Technology Promises to Catapult Tech Sector to New Highs, 100; Finance, Global Financial Crisis Looms: Interest Rates Surge Unexpectedly, -100; Finance, Sudden Surge in Global IPO Activity: Financial Sector Set for Record Profits, +100”"
-
-                                          },
-        ]
-        for key, title_list in titles_dict.items():
-            out_file.write(f"{key}\n")
+def process_titles_average_score(titles_dict):
+    scores_list = []
+    messages = [
+        {"role": "system", "content": system_message},
+    ]
+    sectors = ['Commodities', 'Tech', 'Finance']  # Ordered list of sectors
+    for key in sectors:
+        if key in titles_dict:
+            title_list = titles_dict[key]
             for title in title_list:
                 title = title.strip()
                 if title:
                     messages.append({"role": "user", "content": f"{key}, {title}"})
-                    chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-                    score = chat.choices[0].message.content
-                    out_file.write(score + '\n')
+                    try:
+                        chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+                        score = chat.choices[0].message.content
+                        scores_list.append(int(score))  # Add score to the list
+                        messages.append({"role": "assistant", "content": score})
+                    except Exception as e:
+                        print(f"An error occurred: {e}")
 
-                    messages.append({"role": "assistant", "content": score})
+    #print(scores_list)
+    return sum(scores_list) / len(scores_list) if scores_list else None
+
+
+
+
+
 
 import re
+
 
 def convert_to_int_list(file_path):
     with open(file_path, 'r') as file:
@@ -63,6 +93,7 @@ def calculate_average_each_sector(file_path):
 
     return averages
 
+
 def calculate_overall_average(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
@@ -82,27 +113,24 @@ def calculate_overall_average(file_path):
         return None
 
 
-
-
 openai.api_key = read_api_key('api_key.txt')
 
+input_file = scrap.gather_headlines(2023, 5, 10)
+print("\n")
+scrap.print_results(input_file)
+print("\n")
+#process_titles(input_file)
 
-input_file = {'Tech': ['Microsoft’s $75 Billion Activision Deal Cleared by EU','multiple of CEOs of major Tech companies leaked to all having affairs for years'], 'Finance': ['Berkshire Hathaway Opens New Position in Capital One, Exits BNY Mellon'], 'Commodities': ['How El Niño Could Scramble Commodity Markets']}
-process_titles(input_file)
+print(process_titles_average_score(input_file))
 
-scores_file = 'scores.txt'
-scores = convert_to_int_list(scores_file)
-sector_averages = calculate_average_each_sector(scores_file)
-overall_average = calculate_overall_average(scores_file)
-
-print("'Tech': ['Microsoft’s $75 Billion Activision Deal Cleared by EU','multiple of CEOs of major Tech companies leaked to all having affairs for years'], 'Finance': ['Berkshire Hathaway Opens New Position in Capital One, Exits BNY Mellon'], 'Commodities': ['How El Niño Could Scramble Commodity Markets']")
-print(f'score of each string {scores}')
-print(f'average of each sector {sector_averages}')
-print(f'overall average among all sectors {overall_average}')
-
-
-
-
+# scores_file = 'scores.txt'
+# scores = convert_to_int_list(scores_file)
+# sector_averages = calculate_average_each_sector(scores_file)
+# overall_average = calculate_overall_average(scores_file)
+#
+# print(f'score of each string {scores}')
+# print(f'average of each sector {sector_averages}')
+# print(f'overall average among all sectors {overall_average}')
 
 # Google CEO caught having an joyous affair.
 # Schneider Electric Expands North American Manufacturing
